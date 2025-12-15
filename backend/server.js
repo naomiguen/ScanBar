e8d485c96a30f77f5c2c424ca1d43306b1d44937
guen <naomiguen4@gmail.com>
2025-12-15 22:12:54 +0800

    Fix package json main file and engines

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.json());
 app.use(cors({
     origin: '*',
     credentials: true
